e1752bedd17d67293e53470297a5a281c56295f0
Abdelhakim Jebabra <59029617+HAKIM2000-JE@users.noreply.github.com>
2022-04-27 12:05:05 +0000

    Update profilage.py

--- src/profilage.py
+++ src/profilage.py
@@ -2,14 +2,15 @@ from service import Service
 import pandas as pd
 from datetime import datetime
 from datetime import date
-import environement
+from database import Database
+
+
 class profilage:
+    def __init__(self):
+        Database.initialize()
 
-    def profilage_byAge(self , guestBirthDate):
+    def getProfile_AgeTranche( self ,guestBirthDate , DATA_Guest):
 
-        DataFrame_OnlineChek = Service.get_OnLineChek_DataFrame({})
-        List_DATA = []
-        DATA_Guest={}
         if (isinstance(guestBirthDate, str)):
             Age = abs(date.today().year -
                       datetime.strptime(guestBirthDate, '%Y-%m-%d').year)
@@ -24,6 +25,17 @@ class profilage:
         else:
             DATA_Guest['SCORE_guestAge'] = 2
 
+        return DATA_Guest
+
+
+    def profilage_byAge(self , guestBirthDate ):
+
+
+        DataFrame_OnlineChek =  pd.read_csv("./onLineCheck.csv")
+        List_DATA = []
+        DATA_Guest= {}
+        DATA_Guest= self.getProfile_AgeTranche(guestBirthDate, DATA_Guest)
+
         for index, row in DataFrame_OnlineChek.iterrows():
             DATA= {}
 
@@ -31,20 +43,7 @@ class profilage:
             DATA['guestId'] = row['propertyBookingId']
             # Get age
             guestBirthDate = row['guestBirthDate']
-            if (isinstance(guestBirthDate, str)):
-                Age = abs(date.today().year -
-                          datetime.strptime(guestBirthDate, '%Y-%m-%d').year)
-                if (Age < 20):
-                    DATA['SCORE_guestAge'] = 1
-                elif (Age < 30 and Age >= 20):
-                    DATA['SCORE_guestAge'] = 2
-                elif (Age < 40 and Age >= 30):
-                    DATA['SCORE_guestAge'] = 3
-                else:
-                    DATA['SCORE_guestAge'] = 4
-            else:
-                DATA['SCORE_guestAge'] = 2
-
+            DATA= self.getProfile_AgeTranche(guestBirthDate, DATA)
             List_DATA.append(DATA)
 
         #on utilise le score Age pour identifier chaque tranche d'age(meme score equivalent tranche d'age)
@@ -62,7 +61,7 @@ class profilage:
 
     def profilage_byGender(self , guestGender):
 
-  
+
         DataFrame_OnlineChek = Service.get_OnLineChek_DataFrame({})
 
         #on cherchre les voyageur qui ont le meme genre
