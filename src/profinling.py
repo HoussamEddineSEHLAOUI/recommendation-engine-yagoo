9ca257e2287bc23871f9c808cea7b1272af6e12d
Abdelhakim Jebabra <59029617+HAKIM2000-JE@users.noreply.github.com>
2022-04-28 08:22:23 +0000

    Update profinling.py

--- src/profinling.py
+++ src/profinling.py
@@ -1,29 +1,74 @@
 
+import numpy as np
+
+from service import Service
+import pandas as pd
+from datetime import datetime
+from datetime import date
+from database import Database
 
 class Profiling:
-    def __init__(self, Geust, DataFrameOnLigneChek):
+    def __init__(self, Geust, DataFrameOnLigneChek , DataFramePropertyBooking):
         print('INITIALISE DATA')
         self.Geust = Geust
         self.DataFrameOnLigneChek = DataFrameOnLigneChek
+
 
     def get_Profiles(self):
         print('GET SIMILAR PROFILE')
 
         GUEST = self.Geust
-        SCORE_AGE_GEUST = self.get_ScoreAge(GUEST)
-        SCORE_GENDRE = self.get_ScoreGendre(GUEST)
-        SCORE_RESERVATION_DATE = self.get_ScoreDateReservation(GUEST)
+        SCORE_AGE_GEUST = self.get_ScoreAge(GUEST.guestBirthDate)
+        SCORE_GENDRE = self.get_ScoreGendre(GUEST.guestGender)
+        SCORE_RESERVATION_DATE = self.get_ScoreDate(GUEST.startDate)
+        SCORE_NATIONALITY = self.get_ScoreNatinality(GUEST.guestCountry)
+
+        ListProfiles = []
 
         # 1  faire une seule iteration sur la date frame de online chek    for row in dataframe :
-        # 2  fair cette conndition if Guest match the row of your iteration
-        # if( SCORE_AGE_GEUST == self.ScoreAge(row)  & SCORE_GENDRE== self.get_ScoreGendre(row)  & SSCORE_RESERVATION_DATE ==self.get_ScoreDateReservation(row)):
-        #     add to list profile
+        for index, row in self.DataFrameOnLigneChek.iterrows():
+         # 2  fair cette conndition if Guest match the row of your iteration
+         if (SCORE_AGE_GEUST == self.ScoreAge(row['guestBirthdate']) and SCORE_GENDRE == self.get_ScoreGendre(
+                row['guestGender']) and SCORE_NATIONALITY == self.get_ScoreNatinality(row['guestCountry']) ,   SCORE_RESERVATION_DATE == self.get_ScoreDatey(row['startDate']) ):
 
-    def get_ScoreAge(self, USER):
-        print('RETURN THE SCORE OF AGE ')
+                ListProfiles.append(row['propertyBookingId'])
 
-    def get_ScoreDateReservation(self, USER):
-        print('RETURN THE MONTH OF THE USER : GUEST OR ROW')
+        return ListProfiles
 
-    def get_ScoreGendre(self, USER):
-        print('RETURN THE 0 if man 1 if women')
+    def get_ScoreAge(self, guestBirthDate):
+
+        if (isinstance(guestBirthDate, str)):
+            Age = abs(date.today().year -
+                      datetime.strptime(guestBirthDate, '%Y-%m-%d').year)
+            if (Age < 20):
+                 return 1
+            elif (Age < 30 and Age >= 20):
+                return 2
+            elif (Age < 40 and Age >= 30):
+                return  3
+            else:
+                return  4
+        else:
+            return  2
+
+
+    def get_ScoreNatinality(self, guestCountry):
+
+        return float(guestCountry)
+
+    def get_ScoreDateReservation(self, startDate):
+        if(startDate=='nan'):
+            startDate=np.NaN
+
+        return datetime.strptime(startDate, '%Y-%m-%d').month
+
+    def get_ScoreGendre(self, guestGender):
+
+        if guestGender=='homme':
+            return 0
+        elif    guestGender=='femme':
+            return 1
+        else:
+            return 2
+
+
